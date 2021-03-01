74cb1617968bae2e5a3951541f1bd0b8d4c6a1da
CoderMChu <mch@ihtsdo.org>
2021-03-01 20:56:21 +0000

    CDI-100 Updated tests

--- src/test/java/org/snomed/quality/validator/mrcm/ValidationServiceTest.java
+++ src/test/java/org/snomed/quality/validator/mrcm/ValidationServiceTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.ihtsdo.otf.snomedboot.ReleaseImportException;
 import org.ihtsdo.otf.sqs.service.exception.ServiceException;
@@ -60,26 +61,26 @@ public class ValidationServiceTest {
 		validationService.validateRelease(releaseTestFile, run);
 		assertEquals(267, run.getCompletedAssertions().size());
 		assertEquals(0, run.getSkippedAssertions().size());
-		assertEquals(14, run.getFailedAssertions().size());
-
 		final List<String> failedMessages = Arrays.asList(
-				"The attribute value of 3311482005 |Has presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute group cardinality [0..1]",
-				"The attribute value of 3311482005 |Has presentation strength denominator value concrete (attribute)| must conform to the MRCM concrete attribute data type",
-				"The attribute value of 363698007 |Finding site (attribute)| must conform to the MRCM attribute domain range << 442083009 |Anatomical or acquired body structure (body structure)|",
-				"The attribute value of 3311487004 must conform to the MRCM concrete attribute data type",
-				"The attribute value of 272741003 |Laterality (attribute)| must conform to the MRCM attribute domain range << 182353008 |Side (qualifier value)|",
-				"The attribute value of 363698007 |Finding site (attribute)| must conform to the MRCM attribute group cardinality [0..1]",
 				"The attribute value of 272741003 |Laterality (attribute)| must conform to the MRCM attribute cardinality [0..1]",
-				"The attribute value of 3311483000 must conform to the MRCM concrete attribute data type",
-				"The attribute value of 3311481003 must conform to the MRCM concrete attribute data type",
-				"The attribute value of 3311482005 |Has presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute domain range dec(#10..#20)",
-				"The attribute value of 3311482006 |Has integer presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute domain range int(#30..#40)",
-				"The attribute value of 3311482007 |Has string presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute domain range str(\"test\")",
-				"The attribute value of 3311482007 |Has string presentation strength denominator value concrete (attribute)| must conform to the MRCM concrete attribute data type",
 				"The attribute value of 272741003 |Laterality (attribute)| must conform to the MRCM attribute domain << 91723000 |Anatomical structure (body structure)|",
-				"The attribute value of 3311482006 |Has integer presentation strength denominator value concrete (attribute)| must conform to the MRCM concrete attribute data type");
-
-		run.getFailedAssertions().stream().map(assertion -> failedMessages.contains(assertion.getAssertionText())).forEach(org.junit.Assert::assertTrue);
+				"The attribute value of 3311481003 must conform to the MRCM concrete attribute data type",
+				"The attribute value of 3311482005 |Has presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute in group cardinality [0..1]",
+				"The attribute value of 3311482005 |Has presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute range dec(#10..#20)",
+				"The attribute value of 3311482006 |Has integer presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute range int(#30..#40)",
+				"The attribute value of 3311482006 |Has integer presentation strength denominator value concrete (attribute)| must conform to the MRCM concrete attribute data type",
+				"The attribute value of 3311482007 |Has string presentation strength denominator value concrete (attribute)| must conform to the MRCM attribute range str(\"test\")",
+				"The attribute value of 3311482007 |Has string presentation strength denominator value concrete (attribute)| must conform to the MRCM concrete attribute data type",
+				"The attribute value of 3311483000 must conform to the MRCM concrete attribute data type",
+				"The attribute value of 3311487004 must conform to the MRCM concrete attribute data type",
+				"The attribute value of 363698007 |Finding site (attribute)| must conform to the MRCM attribute in group cardinality [0..1]",
+				"The attribute value of 363698007 |Finding site (attribute)| must conform to the MRCM attribute range << 442083009 |Anatomical or acquired body structure (body structure)|"
+				);
+		assertEquals(failedMessages.size(), run.getFailedAssertions().size());
+		List<String> failedAssertions = run.getFailedAssertions().stream().map(assertion -> assertion.getAssertionText()).collect(Collectors.toList());
+		Collections.sort(failedAssertions);
+//		failedAssertions.stream().forEach(System.out::println);
+		failedMessages.stream().map(assertionText -> failedMessages.contains(assertionText)).forEach(org.junit.Assert::assertTrue);
 	}
 
 	@Test
@@ -148,20 +149,13 @@ public class ValidationServiceTest {
 		assertEquals(88, run.getCompletedAssertions().size());
 		//skipped assertions
 		assertEquals(0, run.getSkippedAssertions().size());
-		assertEquals(5, run.getFailedAssertions().size());
-		List<String> expectedFailed = Arrays.asList("363698007", "3311482005", "272741003", "3311482006", "3311482007");
+		assertEquals(4, run.getFailedAssertions().size());
+		List<String> expectedFailed = Arrays.asList("363698007", "3311482005", "3311482006", "3311482007");
 		for (Assertion assertion : run.getFailedAssertions()) {
-			System.out.println(assertion);
 			assertTrue(expectedFailed.contains(assertion.getAttribute().getAttributeId()));
 			if ("363698007".equals(assertion.getAttribute().getAttributeId())) {
 				assertEquals("<< 442083009 |Anatomical or acquired body structure (body structure)|", assertion.getAttribute().getRangeConstraint());
 				assertTrue(assertion.getCurrentViolatedConceptIds().contains(29857009L));
-			}
-			if ("272741003".equals(assertion.getAttribute().getAttributeId())) {
-				assertEquals("<< 182353008 |Side (qualifier value)|", assertion.getAttribute().getRangeConstraint());
-				assertEquals(2, assertion.getCurrentViolatedConceptIds().size());
-				assertTrue(assertion.getCurrentViolatedConceptIds().contains(91723000L));
-				assertTrue(assertion.getCurrentViolatedConceptIds().contains(113343008L));
 			}
 			if ("3311482005".equals(assertion.getAttribute().getAttributeId()) || "3311482006".equals(assertion.getAttribute().getAttributeId())
 					|| "3311482007".equals(assertion.getAttribute().getAttributeId())) {
